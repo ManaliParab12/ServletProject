61e56da7ec9548d91643a9d535bb06413f079703
ManaliParab12 <manaliparab10@gmail.com>
2021-01-15 12:39:32 +0530

    Created first servlet project

---
+++ src/main/Java/com/firstservlet/FirstServlet.java
@@ -0,0 +1,23 @@
+package com.firstservlet;
+	
+	
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.io.PrintWriter;
+
+@WebServlet("/Hello")
+public class FirstServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("</h3>Hello World, My First Servlet!</h3>");
+		out.close();
+		}
+	}
+
+}
